ca3419e67c5e621966f83b396f0ea2c8703c8dd1
cauacbobato <cauacbobato@gmail.com>
2025-09-16 15:48:14 -0300

    Update atv30.php

--- atv30.php
+++ atv30.php
@@ -14,6 +14,15 @@ Componentes:
  25001459-2 - Eduardo Gritten dos Santos Spohr
  25142481-2 - 
  25356591-2 - Sadrak Araújo Leopoldino
-Data:
-Descritivo:
+Data:16/09/2025
+Descritivo: Esse código verifica se NÃO é fim de semana (usando operador lógico NOT)
 *******************************************************************************/
+
+ // 1 = seg, 2 = ter, 3 = qua, 4 = qui, 5 = sex, 6 = sab, 7 = dom
+$diaSemana = date("N"); 
+
+if (!($diaSemana == 6 || $diaSemana == 7)) {
+    echo "Hoje não é fim de semana.";
+} else {
+    echo "Hoje é fim de semana.";
+}
